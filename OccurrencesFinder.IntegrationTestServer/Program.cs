53184866c0ed8a28b7466aa755cd9df3ccd8c6d7
spidyx <60290098+spidyx@users.noreply.github.com>
2023-02-28 08:13:26 -0500

    feat: Create first Proof of Concept
    It consist of two projects, a console and a web server.
    
    The web server deliver a simple html page on http://localhost:5000
    
    The console application ask the user a URI and a word.
    Then it count how many time the word appear in the source code.
    It does not render a web page, it only GET the URI

---
+++ OccurrencesFinder.IntegrationTestServer/Program.cs
@@ -0,0 +1,20 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
+
+app.MapGet(
+    "/",
+    () => Results.Content(
+        "<html>" +
+        "<head>" +
+        "<meta charset=\"utf-8\" />" +
+        "<title>Hello friend</title>" +
+        "</head>" +
+        "<body>" +
+        "<h1>Hello friend !</h1>" +
+        "<p>This is the only page of this site.</p>" +
+        "<p>This is the second paragraph.</p>" +
+        "</body>" +
+        "</html>",
+        "text/html"));
+
+app.Run();
---
+++ OccurrencesFinder.PoC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+const string DEFAULT_URI = "http://localhost:5000";
+const string DEFAULT_WORD = "hello";
+
+
+Console.WriteLine($"Which web page should we analyze ? (default : {DEFAULT_URI})");
+string? userUri = Console.ReadLine();
+var uri = new Uri(String.IsNullOrEmpty(userUri) ? DEFAULT_URI : userUri);
+
+Console.WriteLine($"Which word should we count ? (default: {DEFAULT_WORD})");
+string userWord = Console.ReadLine() ?? DEFAULT_WORD;
+string word = String.IsNullOrEmpty(userWord) ? DEFAULT_WORD : userWord;
+
+
+using HttpClient httpClient = new();
+using HttpResponseMessage response = await httpClient.GetAsync(uri);
+string resourceContent = await response.Content.ReadAsStringAsync();
+
+var regex = new Regex($"\\b{word}\\b", RegexOptions.IgnoreCase);
+int occurrencesCount = regex.Count(resourceContent);
+
+Console.WriteLine($"We found {occurrencesCount} occurrences of the word {word}");
