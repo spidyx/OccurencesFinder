f7ebb1440233e96d9b3790fef97006fed1a49422
spidyx <60290098+spidyx@users.noreply.github.com>
2023-02-28 10:20:30 -0500

    feat: Create RegexpParser to count word occurences in a text

---
+++ OccurrencesFinder.Core.UnitTests/RegexpParserTests.cs
@@ -0,0 +1,16 @@
+namespace OccurrencesFinder.Core.UnitTests;
+
+public class RegexpParserTests
+{
+    [Fact]
+    public void CountWords_CountExactOccurrences()
+    {
+        RegexpParser regexpParser = new();
+
+        int count = regexpParser.CountWordOccurrences(
+            "word", "This sentence is composed of words, but contains only two times the word word");
+
+
+        count.Should().Be(2);
+    }
+}
---
+++ OccurrencesFinder.Core/RegexpParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OccurrencesFinder.Core;
+
+public class RegexpParser
+{
+    public int CountWordOccurrences(string word, string text) =>
+        Regex.Count(text, $"\\b{word}\\b", RegexOptions.IgnoreCase);
+}
