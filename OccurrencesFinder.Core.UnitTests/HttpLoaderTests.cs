d34f318ec9b29eeeb65cc07596392324026bf84c
spidyx <60290098+spidyx@users.noreply.github.com>
2023-03-01 00:02:06 -0500

    feat: Create HttpLoader that load content through HTTP
    Use RichardSzalay.MockHttp to mock HttpClient

---
+++ OccurrencesFinder.Core.UnitTests/HttpLoaderTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Mime;
+
+namespace OccurrencesFinder.Core.UnitTests;
+
+public class HttpLoaderTests
+{
+    [Fact]
+    public async Task LoadContentAsString()
+    {
+        // Arrange
+        const string EXPECTED_RESULT = "Content from http response";
+        var uri = new Uri("http://non-existent.local");
+
+        var mockHttpMessageHandler = new MockHttpMessageHandler();
+        mockHttpMessageHandler
+            .When(uri.ToString())
+            .Respond(MediaTypeNames.Text.Plain, EXPECTED_RESULT);
+        HttpLoader loader = new(mockHttpMessageHandler.ToHttpClient());
+
+        // Act
+        string result = await loader.LoadContentAsString(uri);
+
+        // Assert
+        result.Should().Be(EXPECTED_RESULT);
+    }
+}
---
+++ OccurrencesFinder.Core/HttpLoader.cs
@@ -0,0 +1,16 @@
+﻿namespace OccurrencesFinder.Core;
+
+public class HttpLoader
+{
+    private readonly HttpClient client;
+    
+    public HttpLoader(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public async Task<string> LoadContentAsString(Uri uri)
+    {
+        return await client.GetStringAsync(uri);
+    }
+}
