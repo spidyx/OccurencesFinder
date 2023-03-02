c3e2073ffd79368c2897471ba287e4556baf74e3
spidyx <60290098+spidyx@users.noreply.github.com>
2023-03-02 14:19:01 -0500

    refactor: remove dead code

--- OccurrencesFinder.Domain/CountWordRecord.cs
+++ OccurrencesFinder.Domain/CountWordRecord.cs
@@ -1,11 +1,3 @@
 ﻿namespace OccurrencesFinder.Domain;
 
 public record CountWordRecord(DateTime DateTime, string Word, int Count);
-
-class temp
-{
-    void toto()
-    {
-        new CountWordRecord(DateTime.Now, null, 0);
-    }
-}
