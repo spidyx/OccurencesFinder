06d47b4ee1c8943160e7f528ba71b005b90d9fa9
spidyx <60290098+spidyx@users.noreply.github.com>
2023-03-02 09:13:40 -0500

    feat(Console): Validate user input
    The word must be non empty
    The uri must be a valid uri

--- OccurrencesFinder.Console/Application.cs
+++ OccurrencesFinder.Console/Application.cs
@@ -24,13 +24,50 @@ public class Application
     private async Task FindOccurrences()
     {
         await output.WriteLineAsync("Which web page should we analyze ?");
-        string uri = await input.ReadLineAsync();
-        
-        await output.WriteLineAsync("Which word should we count ?");
-        string word = await input.ReadLineAsync();
+        Uri uri = await GetInputForUri();
 
-        int count = await countWordOccurrences.Execute(word, new Uri(uri));
-        
+        await output.WriteLineAsync("Which word should we count ?");
+        string word = await GetInputForWord();
+
+        int count = await countWordOccurrences.Execute(word, uri);
+
         await output.WriteAsync(count.ToString());
+    }
+
+    private async Task<Uri> GetInputForUri()
+    {
+        Uri? uri = null;
+
+        do
+        {
+            string? inputUri = await input.ReadLineAsync();
+            try
+            {
+                uri = new Uri(inputUri!);
+            }
+            catch (Exception)
+            {
+                await output.WriteLineAsync("The provided uri has not a correct format. Enter a new uri :");
+            }
+        } while (uri == null);
+
+        return uri;
+    }
+
+    private async Task<string> GetInputForWord()
+    {
+        string? word = null;
+
+        do
+        {
+            string? inputWord = await input.ReadLineAsync();
+
+            if (string.IsNullOrWhiteSpace(inputWord))
+                await output.WriteLineAsync("We did not read a word. Please retry :");
+            else
+                word = inputWord;
+        } while (word == null);
+
+        return word;
     }
 }
